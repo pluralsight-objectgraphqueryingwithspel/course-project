5747839c96a04cfeaaba77aa604b7c575c66fd4c
mhoffman <mike@michaelhoffmaninc.com>
2023-12-14 14:06:14 -0600

    Initial Project

---
+++ src/test/java/com/pluralsight/michaelhoffman/spring/spel/ObjectGraphQueryingWithSpelCourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.michaelhoffman.spring.spel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectGraphQueryingWithSpelCourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
