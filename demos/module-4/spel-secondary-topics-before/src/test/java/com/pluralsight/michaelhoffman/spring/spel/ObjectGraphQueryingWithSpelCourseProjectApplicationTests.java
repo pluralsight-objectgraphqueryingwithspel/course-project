a00f42f34fb021053128bd9725ab5c3d294ec332
mhoffman <mike@michaelhoffmaninc.com>
2023-12-16 07:55:36 -0600

    Updating for multiple modules

---
+++ demos/module-4/spel-secondary-topics-before/src/test/java/com/pluralsight/michaelhoffman/spring/spel/ObjectGraphQueryingWithSpelCourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.michaelhoffman.spring.spel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectGraphQueryingWithSpelCourseProjectApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectGraphQueryingWithSpelCourseProjectApplication.class, args);
+	}
+
+}
